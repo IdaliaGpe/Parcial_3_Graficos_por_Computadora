0b2e766c3fa81ac5221c4a97f47981b5e24e4012
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2022-04-05 09:52:48 -0700

    05/04/22

--- plantilla_shader.py
+++ plantilla_shader.py
@@ -6,5 +6,12 @@ import numpy as np
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-vertex_shader_source = """
+vertex_shader_source = """#version 330 core
+                          layout (location = 0) in vec3 position;
+                          //hay que establecer posición en la propiedad
+                          //gl_Position que es el tipo vec4
+                          
+                          void main() {
+                              gl_Position = vec4(position.x, position.y, position.z, 1.0);
+                          }
                         """
